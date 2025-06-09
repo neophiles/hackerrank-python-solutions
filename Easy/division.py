12dbfa83f67e72fcd674a4df7803ea1c3522ee0f
neophiles <mn.tagle.cs@gmail.com>
2025-06-09 09:39:50 +0800

    Add 'Division' solution

---
+++ Easy/division.py
@@ -0,0 +1,10 @@
+# Problem: Division
+# Difficulty: Easy
+# URL: https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a // b)
+    print(a / b)
